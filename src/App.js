e0b4b95d73f5a615519c924caad5de70deaf742c
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2025-03-21 21:49:50 +0200

    fox1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import { Users } from "./components/Users";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [invites, setInvites] = useState([]);
   const [isLoading, setLoading] = useState(true);
+  const [success, setSuccess] = useState(false);
   const [searchValue, setSearchValue] = useState("");
 
   useEffect(() => {
@@ -29,15 +31,33 @@ function App() {
     setSearchValue(event.target.value);
   };
 
+  const onClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setInvites(invites.filter((item) => item !== id));
+    } else {
+      setInvites([...invites, id]);
+    }
+  };
+
+  const onClickSendInvite = () => {
+    setSuccess(true);
+  };
+
   return (
     <div className="App">
-      <Users
-        onChangeSearchValue={onChangeSearchValue}
-        searchValue={searchValue}
-        items={users}
-        isLoading={isLoading}
-      />
-      {/* <Success /> */}
+      {success ? (
+        <Success count={invites.length} />
+      ) : (
+        <Users
+          onChangeSearchValue={onChangeSearchValue}
+          searchValue={searchValue}
+          items={users}
+          isLoading={isLoading}
+          onClickInvite={onClickInvite}
+          invites={invites}
+          onClickSendInvite={onClickSendInvite}
+        />
+      )}
     </div>
   );
 }
