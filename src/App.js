1214250904c3e14923458078ecba7fe80e553ba2
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2025-03-22 21:48:39 +0200

    fox1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import { Block } from './Block';
-import './index.scss';
+import React from "react";
+import { Block } from "./Block";
+import "./index.scss";
 
 function App() {
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
+      <Block
+        value={0}
+        currency="RUB"
+        onChangeCurrency={(cur) => console.log(cur)}
+      />
       <Block value={0} currency="USD" />
     </div>
   );
